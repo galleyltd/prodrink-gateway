32db58fbcf11986310a77d23e675210bd9948974
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-08-12 18:08:23 +0700

    basic template, auth on postgresql

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace prodrink.gateway
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://localhost:5000")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}

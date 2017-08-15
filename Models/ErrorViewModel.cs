99649aa196575ba6b213cece51dde5efd335afbe
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-08-15 20:44:07 +0700

    changed project to mvc-based with auth

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace prodrink.gateway.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,146 +1,66 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using IdentityServer4;
-using IdentityServer4.Models;
-using IdentityServer4.Postgresql.Entities;
-using IdentityServer4.Postgresql.Extensions;
-using IdentityServer4.Postgresql.Mappers;
-using Marten;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.PlatformAbstractions;
-using Swashbuckle.AspNetCore.Swagger;
+using prodrink.gateway.Data;
+using prodrink.gateway.Models;
+using prodrink.gateway.Services;
 
 namespace prodrink.gateway
 {
-    internal class Startup
+    public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
+        public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
             services.AddMvc();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info {Title = "prodrink API", Version = "v1"});
-            });
-
-            //services.AddSingleton<IClientStore, CustomClientStore>();
-            services.AddIdentityServer()
-                .AddConfigurationStore(Configuration.GetConnectionString("PostgreSql"))
-                .AddOperationalStore()
-                .AddTemporarySigningCredential();
-        }
-
-        private void InitData(IApplicationBuilder app)
-        {
-            var store = DocumentStore.For(Configuration.GetConnectionString("PostgreSql"));
-            store.Advanced.Clean.CompletelyRemoveAll();
-            using (var session = store.LightweightSession())
-            {
-                if (!session.Query<IdentityServer4.Postgresql.Entities.ApiResource>().Any())
-                {
-                    var resources = new List<IdentityServer4.Postgresql.Entities.ApiResource>
-                    {
-                        new IdentityServer4.Postgresql.Entities.ApiResource
-                        {
-                            Name = "api1",
-                            Description = "API",
-                            DisplayName = "api1",
-                            Scopes = new List<ApiScope> {new ApiScope {Name = "api1", DisplayName = "api1"}}
-                        },
-                    };
-                    session.StoreObjects(resources);
-                }
-
-                if (!session.Query<IdentityServer4.Postgresql.Entities.IdentityResource>().Any())
-                {
-                    var resources = new List<IdentityServer4.Postgresql.Entities.IdentityResource>
-                    {
-                        new IdentityResources.OpenId().ToEntity(),
-                        new IdentityResources.Profile().ToEntity(),
-                        new IdentityResources.Email().ToEntity(),
-                        new IdentityResources.Phone().ToEntity()
-                    };
-                    session.StoreObjects(resources);
-                }
-                if (!session.Query<IdentityServer4.Postgresql.Entities.Client>().Any())
-                {
-                    var clients = new List<IdentityServer4.Postgresql.Entities.Client>
-                    {
-                        new IdentityServer4.Postgresql.Entities.Client
-                        {
-                            Id = "api.client",
-                            ClientId = "api.client",
-                            ClientName = "API Client",
-                            AllowedGrantTypes = new List<ClientGrantType>
-                            {
-                                new ClientGrantType {GrantType = GrantType.Hybrid},
-                                new ClientGrantType {GrantType = GrantType.ClientCredentials}
-                            },
-                            AllowedCorsOrigins =
-                                new List<ClientCorsOrigin> {new ClientCorsOrigin {Origin = "http://localhost:5000"}},
-                            RequireClientSecret = true,
-                            ClientSecrets = new List<ClientSecret> {new ClientSecret {Value = "secret".Sha256()}},
-                            RequireConsent = false,
-                            AllowedScopes = new List<ClientScope>
-                            {
-                                new ClientScope {Scope = IdentityServerConstants.StandardScopes.OpenId},
-                                new ClientScope {Scope = IdentityServerConstants.StandardScopes.Profile},
-                                new ClientScope {Scope = "api1"}
-                            },
-                            RedirectUris = new List<ClientRedirectUri>
-                            {
-                                new ClientRedirectUri {RedirectUri = "http://localhost:5000/signin-oidc"}
-                            },
-                            AllowOfflineAccess = true
-                        }
-                    };
-                    session.StoreObjects(clients);
-                }
-                session.SaveChanges();
-            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            InitData(app);
-
-            loggerFactory.AddConsole();
-            app.UseDeveloperExceptionPage();
-
-            app.UseIdentityServer();
-
-            // middleware for google authentication
-            app.UseGoogleAuthentication(new GoogleOptions
+            if (env.IsDevelopment())
             {
-                AuthenticationScheme = "Google",
-                SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme,
-                ClientId = "708996912208-9m4dkjb5hscn7cjrn5u0r4tbgkbj1fko.apps.googleusercontent.com",
-                ClientSecret = "wdfPY6t8H8cecgjlxud__4Gh"
-            });
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
 
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
-            
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "prodrink API V1");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

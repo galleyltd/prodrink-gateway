097814fb03257370ad2c177942c2ba8c198e6976
Konstantin Chuvilyov <f1nzer@outlook.com>
2017-08-23 20:34:49 +0700

    finally fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,6 @@ dotnet publish -c Release -o out
 
 # Build the Docker images
 docker build -t ${PROJECT_NAME} .
-docker tag ${PROJECT_NAME}:latest
 
 # Login to Docker Hub and upload images
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
